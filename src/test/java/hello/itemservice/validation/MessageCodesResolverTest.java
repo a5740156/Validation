7ed91251d7d9a781a8d1a11dd0e377357b9f2ae6
a5740156 <a5740156a@foresys.co.kr>
2024-05-10 17:31:42 +0900

    Valid: BindingResult

---
+++ src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,39 @@
+package hello.itemservice.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MessageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+
+        assertThat(messageCodes).containsExactly("required.item", "required");
+
+    }
+
+    @Test
+    void messageCodesResolverField() {
+
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+        //        BindingResult.rejectValue -> codesResolver를 호출하고 new FiledError()를 생성한다
+        assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+
+    }
+}
